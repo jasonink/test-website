ee879d03c66f706f4deda3b5bfbfb1747419788a
inkink <jasonmagoo3@gmail.com>
2018-02-11 16:11:42 -0800

    added image, expand canvas

--- bounce.js
+++ bounce.js
@@ -1,4 +1,8 @@
 var context;
+var img;
+var width = 600;
+var height = 500;
+var radius = 50;
 var x=100;
 var y=200;
 var dx=5;
@@ -7,21 +11,28 @@ var dy=5;
 function init()
 {
   context= myCanvas.getContext('2d');
-  setInterval(draw,1000/60);
+  //resize canvas to the window size
+  context.canvas.width = width;
+  context.canvas.height = height;
+  img = new Image();
+  img.src = "corgi_face.png";
+  //draw at 60fps
+  setInterval(draw,1000/60);           
 }
 
 function draw()
 {
-  context.clearRect(0,0,300,300);
-  context.beginPath();
-  context.fillStyle="#B9DD49";
-  //Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-  context.arc(x,y,20,0,Math.PI*2,true);
-  context.closePath();
-  context.fill();
+  context.clearRect(0,0,width,height);
+  // context.beginPath();
+  // context.fillStyle="#B9DD49";
+  // //Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+  // context.arc(x,y,radius,0,Math.PI*2,true);
+  // context.closePath();
+  // context.fill();
+  context.drawImage(img,x,y);
   //Boundary logic
-  if (x<20 || x>280) dx=-dx;
-  if (y<20 || y>280) dy=-dy;
+  if (x<0 || x>width - 100) dx=-dx;
+  if (y<0 || y>height - 100) dy=-dy;
   x+=dx;
   y+=dy;
 }
